83b728f973c2fed2fa135de4922b3499752e7f48
Tu4nNguyen <nguyenngoctuan490@gmail.com>
2021-11-12 15:52:39 +0700

    Final

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa2i2fxczCFFCfat5d7',
+        issuer: 'https://dev-38448301.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid','profile','email']
+    }
+}
